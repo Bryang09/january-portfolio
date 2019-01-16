0417526a8694d5abce4e3b23c7e82145ffd71381
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-01-15 21:38:55 -0600

    Updates

---
+++ precache-manifest.4ff70035684a29bfb15a714638c3d0d1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "76bac9269636d6fcbb508e642b1e979f",
+    "url": "/january-portfolio/static/media/new.76bac926.svg"
+  },
+  {
+    "revision": "1b61a6eb49853b720d57162ec0823ba3",
+    "url": "/january-portfolio/static/media/bg3.1b61a6eb.svg"
+  },
+  {
+    "revision": "945f029da36d8b163da8f2c2f91d60f1",
+    "url": "/january-portfolio/static/media/bg2.945f029d.svg"
+  },
+  {
+    "revision": "5d50e2ef7a5fa3540fe3200cd7192361",
+    "url": "/january-portfolio/static/media/bg.5d50e2ef.svg"
+  },
+  {
+    "revision": "c770fcce8685f1c9df94",
+    "url": "/january-portfolio/static/js/runtime~main.c770fcce.js"
+  },
+  {
+    "revision": "be048f459e788fde4b35",
+    "url": "/january-portfolio/static/js/main.be048f45.chunk.js"
+  },
+  {
+    "revision": "802017aa9f949a0ac5dc",
+    "url": "/january-portfolio/static/js/1.802017aa.chunk.js"
+  },
+  {
+    "revision": "be048f459e788fde4b35",
+    "url": "/january-portfolio/static/css/main.dac72fe9.chunk.css"
+  },
+  {
+    "revision": "77d4bd7e169065ffcb03dba982ee05fd",
+    "url": "/january-portfolio/index.html"
+  }
+];
